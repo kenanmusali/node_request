e2c70ecf4524deaa2e82d922c05a2c5846e8779d
kenanmusali <149934213+kenanmusali@users.noreply.github.com>
2025-04-07 14:35:14 +0400

    Delete Task 3/src directory

--- Task 3/src/controller/getListForUser.js
+++
@@ -1,7 +0,0 @@
-const createFileByCar = (req, res) => {
-    const { platenumber } = req.query;
-    const file = req.file;
-    res.send('File uploaded successfully');
-};
-
-module.exports = { createFileByCar };
